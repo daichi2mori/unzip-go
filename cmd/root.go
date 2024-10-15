506b273a4e15a0cc1d5ddf985fef1a26970ade00
daichi2mori <daichi2mori@gmail.com>
2024-10-15 23:54:50 +0900

    first commit

---
+++ cmd/root.go
@@ -0,0 +1,58 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	version string
+}
+
+var config Config
+var cfgFile string = "config.yml"
+var versionFlag bool
+
+var rootCmd = &cobra.Command{
+	Use:   "zgo",
+	Short: "解凍や圧縮のフォルダを指定して実行できます",
+	Long:  `解凍や圧縮のフォルダを指定して実行できます`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: func(cmd *cobra.Command, args []string) {
+		if versionFlag {
+			version := viper.GetString("version")
+			if version == "" {
+				fmt.Println("バージョン情報が見つかりません")
+			} else {
+				fmt.Println("バージョン: ", version)
+			}
+			os.Exit(0)
+		}
+	},
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func initConfig() {
+	viper.SetConfigFile(cfgFile)
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	viper.ConfigFileUsed()
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "zgo/cmd"
+
+func main() {
+	cmd.Execute()
+}
